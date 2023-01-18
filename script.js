f849d7d93d32ccac1bb25bcdb2e120e649df3ca4
Chey Rose <cheyroseflammer@gmail.com>
2023-01-18 08:20:45 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+const tl = gsap.timeline();
+
+tl.from('.intro > p', 1.8, {
+  opacity: 0,
+  y: -100,
+  ease: 'power4.out',
+  delay: 1,
+  stagger: {
+    amount: 0.3,
+  },
+});
+
+tl.from(
+  '.hr',
+  1.8,
+  {
+    width: 0,
+    ease: 'power4.out',
+    delay: 0,
+    stagger: {
+      amount: 0.3,
+    },
+  },
+  '-=1.5'
+);
+tl.from(
+  '.reveal h1',
+  1.8,
+  {
+    y: 200,
+    ease: 'power4.out',
+    stagger: {
+      amount: 0.8,
+    },
+  },
+  '-=2'
+);
+tl.from(
+  '.nav-item',
+  1.8,
+  {
+    opacity: 0,
+    y: 100,
+    ease: 'power4.out',
+    stagger: {
+      amount: 0.3,
+    },
+  },
+  '-=2'
+);
