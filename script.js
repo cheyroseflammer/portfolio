33a50c044a48562b0976656f2009fc55af6e7bcf
Chey Rose <cheyroseflammer@gmail.com>
2023-01-22 08:53:36 -0800

    jquery browser resize

--- script.js
+++ script.js
@@ -48,3 +48,15 @@ tl.from(
   },
   '-=2'
 );
+
+jQuery(function ($) {
+  var windowWidth = $(window).width();
+  var windowHeight = $(window).height();
+
+  $(window).resize(function () {
+    if (windowWidth != $(window).width() || windowHeight != $(window).height()) {
+      location.reload();
+      return;
+    }
+  });
+});
